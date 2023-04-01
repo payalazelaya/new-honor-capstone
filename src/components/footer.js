8f4c4d761e4495a0fd7950411d3a3d2bb0c3810e
Paola Ayala <paolaaz235@gmail.com>
2023-04-01 18:20:26 -0400

    Edit footer + remove bkgd color

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        2023 Honors College Capstone Project{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by Paola Ayala Zelaya
       </p>
     </div>
   </footer>
